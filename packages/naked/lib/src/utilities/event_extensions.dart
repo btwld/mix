bc832d12ceb601416d22c14c6368d30a41fd5aad
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2025-05-13 21:06:08 -0300

    use FocusableActionDetector in button

--- packages/naked/lib/src/utilities/event_extensions.dart
+++ packages/naked/lib/src/utilities/event_extensions.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 
 extension KeyEventExtensions on KeyEvent {
   bool get isSpaceOrEnter =>
@@ -9,24 +8,4 @@ extension KeyEventExtensions on KeyEvent {
   bool get isDown => this is KeyDownEvent;
   bool get isUp => this is KeyUpEvent;
   bool get isEscape => logicalKey == LogicalKeyboardKey.escape;
-}
-
-mixin KeyboardActionHandler {
-  ValueChanged<bool>? get onPressedState;
-  VoidCallback get onkeyAction;
-
-  KeyEventResult handleOnKeyEvent(FocusNode focusNode, KeyEvent event) {
-    if (event.isDown && event.isSpaceOrEnter) {
-      onPressedState?.call(true);
-
-      return KeyEventResult.handled;
-    } else if (event.isUp && event.isSpaceOrEnter) {
-      onPressedState?.call(false);
-      onkeyAction.call();
-
-      return KeyEventResult.handled;
-    }
-
-    return KeyEventResult.ignored;
-  }
 }
