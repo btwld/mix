2c101835899885b199e945725aad2af7e6d59a8d
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2025-06-18 21:02:43 -0300

    Update chip.dart

--- packages/remix/lib/src/components/content_presentation/chip/chip.dart
+++ packages/remix/lib/src/components/content_presentation/chip/chip.dart
@@ -4,7 +4,6 @@ import 'package:mix/mix.dart';
 import 'package:mix_annotations/mix_annotations.dart';
 import 'package:naked/naked.dart';
 
-import '../../../core/attributes/icon_theme_data.dart';
 import '../../../helpers/mix_controller_mixin.dart';
 import '../../../helpers/remix_builder.dart';
 
