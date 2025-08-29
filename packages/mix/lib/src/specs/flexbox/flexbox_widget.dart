fc55cc50ec2cc1484162d8c1eed837d9144ec1fd
Leo Farias <leoafarias@gmail.com>
2025-08-29 16:00:41 -0400

    refactor: Reorder import statements for consistency in flexbox_widget.dart

--- packages/mix/lib/src/specs/flexbox/flexbox_widget.dart
+++ packages/mix/lib/src/specs/flexbox/flexbox_widget.dart
@@ -2,9 +2,9 @@
 
 import 'package:flutter/widgets.dart';
 
+import '../../core/style_builder.dart';
 import '../../core/style_widget.dart';
 import '../../core/widget_spec.dart';
-import '../../core/style_builder.dart';
 import '../box/box_widget.dart';
 import '../flex/flex_spec.dart';
 import 'flexbox_spec.dart';
