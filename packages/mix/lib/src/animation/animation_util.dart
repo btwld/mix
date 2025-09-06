3284488ec71a874bc21b34924a678254650732ec
Leo Farias <leoafarias@gmail.com>
2025-09-06 14:29:53 -0400

    feat: Enhance documentation for AnimationConfigUtility and SpringCurve classes

--- packages/mix/lib/src/animation/animation_util.dart
+++ packages/mix/lib/src/animation/animation_util.dart
@@ -4,16 +4,23 @@ import '../core/style.dart';
 import '../core/utility.dart';
 import 'animation_config.dart';
 
+/// A utility for creating [AnimationConfig] instances.
 class AnimationConfigUtility<T extends Style<Object?>>
     extends MixUtility<T, AnimationConfig> {
   const AnimationConfigUtility(super.utilityBuilder);
 
+  /// Creates an [AnimationConfig] with the given [duration] and [curve].
+  ///
+  /// This is used for implicit animations.
   T implicit({required Duration duration, Curve curve = Curves.linear}) {
     return utilityBuilder(
       CurveAnimationConfig(duration: duration, curve: curve),
     );
   }
 
+  /// Creates an [AnimationConfig] with the given [duration] and [curve].
+  ///
+  /// This is used for implicit animations.
   T call({required Duration duration, Curve curve = Curves.linear}) {
     return implicit(duration: duration, curve: curve);
   }
--- packages/mix/lib/src/animation/spring_curves.dart
+++ packages/mix/lib/src/animation/spring_curves.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/physics.dart';
 import 'package:flutter/widgets.dart';
 
+/// A curve that uses a spring simulation to animate.
 class SpringCurve extends Curve {
   final Tolerance tolerance = Tolerance.defaultTolerance;
   final SpringSimulation _sim;
 
+  /// Creates a spring curve with the given [mass], [stiffness], and [damping].
   SpringCurve({
     double mass = 1.0,
     double stiffness = 180.0,
@@ -16,6 +18,7 @@ class SpringCurve extends Curve {
          0.0,
        );
 
+  /// Creates a spring curve with the given [mass], [stiffness], and [ratio].
   SpringCurve.withDampingRatio({
     double mass = 1.0,
     double stiffness = 180.0,
@@ -31,6 +34,7 @@ class SpringCurve extends Curve {
          0.0,
        );
 
+  /// Creates a spring curve with the given [duration] and [bounce].
   SpringCurve.withDurationAndBounce({
     Duration duration = const Duration(milliseconds: 500),
     double bounce = 0.0,
