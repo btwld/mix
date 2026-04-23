63ba537aa5fa4b40d4cb256b89119473887c156a
Lucas Oliveira <tilucasoli@Gmail.com>
2026-04-23 10:49:40 -0300

    test(mix_generator): validation cases for MixWidgetGenerator

---
+++ packages/mix_generator/test/integration/mix_widget_generator_validation_test.dart
@@ -0,0 +1,88 @@
+import 'package:build/build.dart';
+import 'package:build_test/build_test.dart';
+import 'package:mix_generator/mix_generator.dart';
+import 'package:source_gen/source_gen.dart';
+import 'package:test/test.dart';
+
+Builder _partBuilder(Generator generator) =>
+    PartBuilder([generator], '.g.dart');
+
+Future<void> _expectInvalid(String source, String messageFragment) async {
+  final result = await testBuilder(
+    _partBuilder(const MixWidgetGenerator()),
+    {'mix_generator|lib/case.dart': source},
+    generateFor: {'mix_generator|lib/case.dart'},
+    onLog: (_) {},
+  );
+  expect(result.succeeded, isFalse);
+  expect(result.errors.join('\n'), contains(messageFragment));
+}
+
+void main() {
+  group('MixWidgetGenerator validation', () {
+    test('rejects annotation on a class', () async {
+      const source = r'''
+library c;
+part 'case.g.dart';
+class MixWidget { final String name; final bool stylable; const MixWidget(this.name, {this.stylable = false}); }
+@MixWidget('Bad')
+class NotAllowed {}
+''';
+      await _expectInvalid(source, 'top-level variables or functions');
+    });
+
+    test('rejects non-Style type', () async {
+      const source = r'''
+library c;
+part 'case.g.dart';
+class MixWidget { final String name; final bool stylable; const MixWidget(this.name, {this.stylable = false}); }
+class NotAStyler { const NotAStyler(); NotAStyler call() => this; }
+@MixWidget('Bad')
+final bad = const NotAStyler();
+''';
+      await _expectInvalid(source, 'Style<T> subtype');
+    });
+
+    test('rejects styler without call()', () async {
+      const source = r'''
+library c;
+part 'case.g.dart';
+class MixWidget { final String name; final bool stylable; const MixWidget(this.name, {this.stylable = false}); }
+class Style<T> { const Style(); }
+class BoxSpec {}
+class NoCallStyler extends Style<BoxSpec> { const NoCallStyler(); }
+@MixWidget('Bad')
+final bad = const NoCallStyler();
+''';
+      await _expectInvalid(source, 'call(...)');
+    });
+
+    test('rejects stylable with style parameter on function', () async {
+      const source = r'''
+library c;
+part 'case.g.dart';
+class MixWidget { final String name; final bool stylable; const MixWidget(this.name, {this.stylable = false}); }
+class Style<T> { const Style(); }
+class BoxSpec {}
+class Key { const Key(); }
+class BuildContext {}
+abstract class Widget { const Widget({Key? key}); }
+abstract class StatelessWidget extends Widget { const StatelessWidget({Key? key}) : super(key: key); Widget build(BuildContext context); }
+class SizedBox extends StatelessWidget { const SizedBox({Key? key}) : super(key: key); @override Widget build(BuildContext context) => this; }
+class Box extends StatelessWidget {
+  const Box({Key? key, Object? style, Widget? child}) : super(key: key);
+  @override
+  Widget build(BuildContext context) => const SizedBox();
+}
+class BoxStyler extends Style<BoxSpec> {
+  const BoxStyler();
+  BoxStyler merge(BoxStyler? other) => this;
+  Box call({Key? key, Widget? child}) => Box(key: key, style: this, child: child);
+}
+@MixWidget('Bad', stylable: true)
+BoxStyler build({required BoxStyler style}) => style;
+''';
+      await _expectInvalid(source, 'reserves the `style`');
+    });
+  });
+}
