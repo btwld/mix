0912ec3ae2b411b0a62cc5e960c45ae213c4fa5f
Leo Farias <leoafarias@gmail.com>
2026-05-28 12:40:25 -0400

    chore(mix): default StyleWidget to IdentityStyle<Spec> (#921)

--- packages/mix/lib/src/specs/text/text_widget.dart
+++ packages/mix/lib/src/specs/text/text_widget.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 import '../../core/directive.dart';
+import '../../core/style.dart';
 import '../../core/style_widget.dart';
 import 'text_spec.dart';
-import 'text_style.dart';
 
 /// [StyledText] is equivalent to Flutter's [Text] widget, but provides
 /// styling capabilities through the Mix framework. It allows you to create
 /// visually styled text with typography, colors, decorations, text
 /// transformations, and responsive variants.
 ///
-/// You can use [TextStyler] to create styles with a fluent API. Example:
+/// You can use `TextStyler` to create styles with a fluent API. Example:
 ///
 /// ```dart
 /// final style = TextStyler()
@@ -27,7 +27,7 @@ class StyledText extends StyleWidget<TextSpec> {
   /// Creates a [StyledText] with required [text] and optional [style] or [spec].
   const StyledText(
     this.text, {
-    super.style = const TextStyler.create(),
+    super.style = const IdentityStyle(TextSpec()),
     super.styleSpec,
     super.key,
   });
